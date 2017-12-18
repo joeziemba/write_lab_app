9be9ae7c3f9dce0e476b8b011ab0cc6728a2beec
Joe Ziemba <jziemba90@gmail.com>
2017-12-18 12:30:15 -0500

    added Boards

--- app/models/author.rb
+++ app/models/author.rb
@@ -3,4 +3,13 @@ class Author < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  # Associations
+  # has_many :boards
+  # has_many :characters
+  # has_many :posts, through :characters
+  # has_many :threads, through :characters
+
+  # Validations
+  validates_presence_of :username, :email, :password
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171218154444) do
+ActiveRecord::Schema.define(version: 20171218171359) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,15 @@ ActiveRecord::Schema.define(version: 20171218154444) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_authors_on_email", unique: true
     t.index ["reset_password_token"], name: "index_authors_on_reset_password_token", unique: true
+  end
+
+  create_table "boards", force: :cascade do |t|
+    t.string "name", null: false
+    t.text "description", default: "", null: false
+    t.bigint "author_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["author_id"], name: "index_boards_on_author_id"
   end
 
 end

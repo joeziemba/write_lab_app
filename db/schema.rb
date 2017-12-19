020ec3f02e8dfc096401f9b50e174077b0f6b09e
Joe Ziemba <jziemba90@gmail.com>
2017-12-19 14:33:49 -0500

    added image to boards table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171218202606) do
+ActiveRecord::Schema.define(version: 20171219192223) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(version: 20171218202606) do
     t.bigint "author_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image", default: "", null: false
     t.index ["author_id"], name: "index_boards_on_author_id"
   end
 

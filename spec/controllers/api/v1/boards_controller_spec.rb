026a449c44d0c158b3f9c58e487965df4d076ede
Joe Ziemba <jziemba90@gmail.com>
2017-12-20 10:20:42 -0500

    testing for Boards Api controller

---
+++ spec/controllers/api/v1/boards_controller_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+describe Api::V1::BoardsController, type: :controller do
+  let!(:testBoard) { create(:board) }
+
+  it 'should return a json response' do
+    get :show, params: { id: testBoard.id }
+
+    expect(response.status).to eq(200)
+    expect(response.header['Content-Type']).to include 'application/json'
+  end
+
+  it 'should include the board name and description' do
+    get :show, params: { id: testBoard.id }
+
+    data = JSON.parse(response.body)
+    expect(data['name']).to eq('New Hogwarts')
+    expect(data['description']).to eq("The Battle of Hogwarts and The Dark Lord Voldemort are now more legend than history. The magical world moved on to great prosperity in the wake of those events, but peace never last. Murmurs of a new dark force are beginning to ripple through Britain's wizarding community. And Hogwarts once again seems to be at risk.")
+  end
+end
--- spec/factories/boards.rb
+++ spec/factories/boards.rb
@@ -1,6 +1,7 @@
 FactoryBot.define do
   factory :board do
     name "New Hogwarts"
+    description "The Battle of Hogwarts and The Dark Lord Voldemort are now more legend than history. The magical world moved on to great prosperity in the wake of those events, but peace never last. Murmurs of a new dark force are beginning to ripple through Britain's wizarding community. And Hogwarts once again seems to be at risk."
     author
   end
 end

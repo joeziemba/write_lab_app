de40023268f3a723966b8f44dc9eaee4e6dcd98b
Joe Ziemba <jziemba90@gmail.com>
2017-12-22 12:26:45 -0500

    codeclimate fixes

--- app/controllers/api/v1/boards_controller.rb
+++ app/controllers/api/v1/boards_controller.rb
@@ -1,13 +1,12 @@
 class Api::V1::BoardsController < ApplicationController
   def show
     @board = Board.find(params[:id])
-    @user = current_author
     @username = ''
     @characters = [{ id: 0, name: '' }]
 
     if current_author
-      @username = @user.username
-      if !@user.characters.empty?
+      @username = current_author.username
+      if !current_author.characters.empty?
         @characters = @user.characters.select { |c| c.board_id == @board.id }
       end
     end

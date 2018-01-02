ec282f8ffb54e6a2d6fdbcca99b5314799a52add
Joe Ziemba <jziemba90@gmail.com>
2018-01-02 14:51:10 -0500

    changed idons to FA5 versions

--- react/src/components/BoardSidebar.js
+++ react/src/components/BoardSidebar.js
@@ -9,7 +9,7 @@ const BoardSidebar = (props) => {
   }
   let editBoard;
   if(props.currentAuthor == props.boardAuthorId) {
-    editBoard = <a href={`/boards/${props.boardId}/edit`} className='util-button-dark'><i className="fas fa-edit"></i> Edit Board</a>
+    editBoard = <a href={`/boards/${props.boardId}/edit`} className='util-button-dark'><i className="fa fa-pencil"></i>&nbsp; Edit Board</a>
   }
   return(
     <div className='cell large-5 medium-5 hide-for-small-only' id='board-sidebar'>
